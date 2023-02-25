c61e8086cd9100616d8b342473456462839ccd5d
syed <s.muazzam.siddiqhi@gmail.com>
2023-02-26 01:24:27 +0530

    Update ScrollToTop.js
    Updated

--- src/components/common/ScrollToTop.js
+++ src/components/common/ScrollToTop.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import "./ScrollToTop.css"; // import custom CSS file for styling
 
 function ScrollToTop() {
   const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  return null;
+
+  return null; // returning null because this component doesn't have any visible UI
 }
 
 export default ScrollToTop;
