974dc7612ac07727505a4802a19fedb1e3d3ce58
syed <s.muazzam.siddiqhi@gmail.com>
2023-02-26 02:06:53 +0530

    Update Home.js
    Updated

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,80 +2,61 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../../common/Particle";
+import Home2 from "./Home2";
 import Type from "./Type";
+import Projects from '../Projects/Projects.js'
+import About from '../About/About'
+import Resume from '../Resume/Resume.js'
+import { DeveloperAnimation } from '../../../lotties/Lotties';
+import Tilt from "react-parallax-tilt";
+import { AiOutlineDownload } from "react-icons/ai";
 import Card from "react-bootstrap/Card";
+import Copy from "../../common/Copy";
 import { ImPointRight } from "react-icons/im";
 
 function Home() {
+  const pdf = 'https://drive.google.com/file/d/1trsOB4sbv5gKRQp-bzmmFYhwvPZfBC0g/view?usp=sharing'
+  const cal = 'https://calendar.ai/syed'
+  const dwn = 'https://docs.google.com/document/d/1XvIdphkNLPkICvHjXUbf2pneT9tc472I/edit?usp=sharing&ouid=112689532247814358148&rtpof=true&sd=true'
   return (
-    <section>
-      <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }} className="home-section" id="home">
+    <section style={{ background: "#14161F" }}>
+      <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-          <Row className="justify-content-center">
-            <Col md={5} xs={12} sm={12} className="home-picture">
+          <Row>
+            <Col md={5} xs={12} style={{ position: "relative" }}>
               <div className="developer-animation">
-                <img src="/images/home-pic.jpg" alt="home pic" />
+                <Tilt>
+                  <DeveloperAnimation />
+                </Tilt>
               </div>
             </Col>
-            <Col xs={12} className="home-header">
+            <Col xs={12} md={7} className="home-header">
               <h1 className="heading">
-                Welcome to Indigo Prime International
+                Indigo Prime International
               </h1>
-              <div className="main-name">
-                <p>
-                  We are a leading export company based in India, dealing in a wide range of products including textiles, handicrafts, home decor, and more.
+              <p className="sub-heading">
+                Your trusted partner in international trade
+              </p>
+              <div className="main-content">
+                <p className="description">
+                  We specialize in export/import business and offer end-to-end solutions for all your trade requirements. Our team of experts provides personalized services to help you navigate through the complexities of international trade and make it hassle-free for you. We work with a wide range of industries and ensure timely delivery of goods with the highest level of quality assurance. Get in touch with us today to explore the opportunities in the global market.
                 </p>
-                <p>
-                  With over 10 years of experience in the industry, we have established ourselves as a reliable and trusted exporter. Our commitment to quality, customer satisfaction, and ethical business practices sets us apart from the competition.
-                </p>
-                <p>
-                  Whether you are a wholesaler or a retailer, we have the expertise and resources to fulfill your export needs. Contact us today to discuss how we can help grow your business.
-                </p>
-                <Button variant="primary" className="action-btn" href="#contact">
-                  Contact Us
-                </Button>
+                <Row>
+                  <Col>
+                    <div className="buttons">
+                      <Button variant="primary" className="action-btn" href={pdf} target="_blank">
+                        <AiOutlineDownload />
+                        &nbsp; Get Our Profile
+                      </Button>
+                      <Button variant="outline-primary" className="action-btn" href={cal} target="_blank">
+                        <AiOutlineDownload />
+                        &nbsp; Schedule a Meeting
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
               </div>
-            </Col>
-          </Row>
-          <Row className="home-skills">
-            <Col md={12} xs={12}>
-              <h1 className="heading">
-                Our Products
-              </h1>
-            </Col>
-            <Col md={4} xs={12}>
-              <Card className="quote-card-view">
-                <Card.Body>
-                  <blockquote className="blockquote mb-0">
-                    <p>
-                      <ImPointRight /> Textiles
-                    </p>
-                  </blockquote>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={4} xs={12}>
-              <Card className="quote-card-view">
-                <Card.Body>
-                  <blockquote className="blockquote mb-0">
-                    <p>
-                      <ImPointRight /> Handicrafts
-                    </p>
-                  </blockquote>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={4} xs={12}>
-              <Card className="quote-card-view">
-                <Card.Body>
-                  <blockquote className="blockquote mb-0">
-                    <p>
-                      <ImPointRight /> Home Decor
-                    </p>
-                  </blockquote>
-                </Card.Body>
-              </Card>
             </Col>
           </Row>
         </Container>
@@ -85,3 +66,21 @@ function Home() {
 }
 
 export default Home;
+
+// CSS rules
+.home-section {
+  height: 100vh;
+  padding-top: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.developer-animation {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
