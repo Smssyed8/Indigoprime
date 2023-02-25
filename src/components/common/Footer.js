2e31c6e81fd16c4ffd421ef1a81a7038dbffc19a
syed <s.muazzam.siddiqhi@gmail.com>
2023-02-26 02:12:40 +0530

    Update Footer.js
    Ups

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,40 +1,47 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
-import "./Footer.css"; // import custom CSS file for styling
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
+  
   return (
-    <Container fluid className="footer-container"> {/* added a custom CSS class for styling */}
+    <Container fluid className="footer">
       <Row>
-        <Col md={4} className="footer-content">
-          <h3 className="footer-heading">Designed and Developed by Syed Ms</h3> {/* added a custom CSS class for styling */}
+        <Col md={4} className="footer-copywright">
+          <h3>Designed and Developed by Syed Ms</h3> 
         </Col>
-        <Col md={4} className="footer-content">
-          <h3 className="footer-heading">© {year} Indigo Prime. All rights reserved.</h3> {/* updated copyright text */}
+        <Col md={4} className="footer-copywright">
+          <h3>Copyright © {year} </h3>
         </Col>
-        <Col md={4} className="footer-content">
-          <ul className="social-icons-list"> {/* added a custom CSS class for styling */}
-            <li>
-              <a href="https://github.com/Smssyed8/" className="social-icon-link">
-                <AiFillGithub className="social-icon" />
+        <Col md={4} className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Smssyed8/"
+                style={{ color: "white" }}
+              >
+                <AiFillGithub />
               </a>
             </li>
-            <li>
-              <a href="https://www.linkedin.com/in/syedmuazzamsiddiqhi/" className="social-icon-link">
-                <FaLinkedinIn className="social-icon" />
+            
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/syedmuazzamsiddiqhi/"
+                style={{ color: "white" }}
+              >
+                <FaLinkedinIn />
               </a>
             </li>
-            <li>
-              <a href="https://www.instagram.com/iamsyedms/" className="social-icon-link">
-                <AiFillInstagram className="social-icon" />
+            
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/iamsyedms/"
+                style={{ color: "white" }}
+              >
+                <AiFillInstagram />
               </a>
             </li>
           </ul>
